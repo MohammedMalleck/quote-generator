afa6308ebded58b876c5a0928981e9d23c2983e8
Mohammed Malleck <mohammedmalleck@gmail.com>
2024-05-11 19:38:46 +0400

    Version 3

--- script.js
+++ script.js
@@ -19,3 +19,73 @@ document.querySelector('.new-quote-button').addEventListener('click',()=>{
   document.querySelector('.author-name').innerHTML = 'Loading';
   getAndDisplayQuote();
 });
+
+
+
+document.querySelector('.toolbar > button:nth-child(1)').addEventListener('click',()=>{
+  handleTextToSpeech();
+});
+
+function handleTextToSpeech(){
+  const playBtn = document.querySelector('.toolbar > button:nth-child(1)');
+  playBtn.classList.toggle('active');
+
+  const speech = new SpeechSynthesisUtterance();
+  const textContent = document.querySelector('.text span').textContent;
+  const synth = window.speechSynthesis;
+  const voices = synth.getVoices();
+
+  speech.text = textContent;
+  speech.voice = voices[0];
+  speech.rate = 1;
+  synth.speak(speech);
+
+};
+
+class CopyBtn{
+  #timeout = false;
+  #button
+
+  constructor(button){
+    this.#button = button;
+  }
+
+  addEvent(){
+    this.#button.addEventListener('click',()=>{
+
+      if(this.#timeout){
+        clearTimeout(this.#timeout);
+        this.#timeout = false;
+      }else{
+        this.#button.classList.add('active');
+      }
+    
+      this.#timeout = setTimeout(()=>{
+        this.#button.classList.remove('active');
+        this.#timeout = false;
+      },1000);
+    
+      const quote = document.querySelector('.text span').textContent;
+      const authorName = document.querySelector('.author-name').textContent;
+      const string = `"${quote}" - ${authorName}`;
+      navigator.clipboard.writeText(string);
+      
+    });
+  };
+};
+
+new CopyBtn(document.querySelector('.toolbar > button:nth-child(2)')).addEvent();
+
+document.querySelector('.toolbar > button:nth-child(3)').addEventListener('click',()=>{
+  const shareBtn = document.querySelector('.toolbar > button:nth-child(3)');
+  shareBtn.classList.toggle('active');
+});
+
+document.querySelector('.js-twitter-icon').addEventListener('click',()=>{
+  const quote = document.querySelector('.text span').textContent;
+  const authorName = document.querySelector('.author-name').textContent;
+  const string = `"${quote}" - ${authorName}`;
+
+  window.open(`https://twitter.com/intent/tweet?text=${string}` , "Tweet Window" , "width=600 , height=300" );
+});
+
